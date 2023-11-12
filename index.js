ea368bcb8cb5a02cc0679ea2a7a1ee91885cfea3
Airton Lima <jose.airtonlimajr@hotmail.com>
2023-11-12 20:28:58 -0300

    customização para docelar ofertas

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ require('dotenv').config();
 const {PythonShell} = require('python-shell')
 
 async function testPy() {
-    var imglink = "https://m.media-amazon.com/images/I/61auvO7PuuL._SL500_.jpg";
-    var link = 'https://google.com.br'
+    var imglink = "https://m.media-amazon.com/images/I/71xoR4A6q-L._AC_SY450_.jpg";
+    var link = 'https://amzn.to/49w7rHe'
 
-    let options = {
-        args: [process.env.LOGIN, process.env.SENHA, imglink, "R$ XXX,XX", "(XXX% off)", "R$ XXX,XX", "Desconsiderar", "Favor desconsiderar esse story é um teste.", link]
-    }
+    const { IG_USERNAME, IG_PASSWORD } = process.env;
+
+    const options = {
+        mode: 'text',
+        args: [IG_USERNAME, IG_PASSWORD, imglink, "R$ 386,10", "(10% off)", "R$ 100,00", "Echo Dot 5ª geração | O Echo Dot com o melhor som já lançado | Cor Preta", "Favor desconsiderar esse story é um teste.", link]
+    };
 
     try {
-        PythonShell.run('./src/python/instagram.py', options, function(err, results) {
+        PythonShell.run('./src/python/instagram.py', options, (err, results) => {
+            if (err) console.log(err);
             console.log(results)
-            for (const result of results) {
-                if (result.includes('Erro:')) {
-                    console.log(result)
-                }
-            }
+            // for (const result of results) {
+            //     if (result.includes('Erro:')) {
+            //         console.log(result)
+            //     }
+            // }
         })
     } catch (err) {
         console.log(err.message)
--- src/python/instagram.py
+++ src/python/instagram.py
@@ -10,8 +10,8 @@ import os
 from instagrapi import Client
 from instagrapi.types import StoryLink
 
-# IG_USERNAME = "gibipromo"
-# IG_PASSWORD = "Setembro@27"
+# IG_USERNAME = 'doce.lar.ofertas'
+# IG_PASSWORD = 'D8xe321u9s#'
 # IMAGE_LINK = "https://m.media-amazon.com/images/I/61auvO7PuuL._SL500_.jpg"
 # PRECO = "R$ XXX,XX"
 # DESCONTO = "(XXX% off)"
@@ -19,7 +19,7 @@ from instagrapi.types import StoryLink
 # TITULO = "Desconsiderar"
 # DESCRICAO = "Favor desconsiderar esse story, ele é um teste. Com uma descrição muito muito grande para ver como fica na imagem e letras pequenas para não quebrar."
 # LINK = 'https://google.com.br'
-
+print(sys.argv)
 IG_USERNAME = sys.argv[1]
 IG_PASSWORD = sys.argv[2]
 IMAGE_LINK = sys.argv[3]
@@ -37,15 +37,15 @@ def login():
     global cl
     
     try:
-        if os.path.exists(IG_CREDENTIAL_PATH):
-            cl.load_settings(IG_CREDENTIAL_PATH)
-            cl.login(IG_USERNAME, IG_PASSWORD)
-        else:
-            cl.login(IG_USERNAME, IG_PASSWORD)
-            cl.dump_settings(IG_CREDENTIAL_PATH)
+        # if os.path.exists(IG_CREDENTIAL_PATH):
+        #     cl.load_settings(IG_CREDENTIAL_PATH)
+        #     cl.login(IG_USERNAME, IG_PASSWORD)
+        # else:
+        cl.login(IG_USERNAME, IG_PASSWORD)
+        cl.dump_settings(IG_CREDENTIAL_PATH)
         return True
-    except:
-        print("Erro: Erro ao logar")
+    except Exception as ex:
+        print("Erro: Erro ao logar", ex)
         return False
 
 def save_image():
@@ -57,82 +57,85 @@ def save_image():
             print('Erro: Erro ao requerir imagem')
 
         bg = Image.open('./src/python/res/story.jpg')
-        image = Image.open(BytesIO(response.content))
-        image = image.resize((322, 430))
-        link = Image.open('./src/python/res/link.png')
+        productImage = Image.open(BytesIO(response.content))
+        productImage = productImage.resize((600, 600))
+        linkImage = Image.open('./src/python/res/link.jpg')
+        linkImage = linkImage.resize((600, 120))
 
         imgcp = bg.copy()
-        imgcp.paste(image, ((199), (311)))
-        imgcp.paste(link, ((100), (1050)), link)
+        imgcp.paste(productImage, ((150), (250)))
+        imgcp.paste(linkImage, ((150), (1200)))
 
         draw = ImageDraw.Draw(imgcp)
-        pos_y = 110
-        pos_x = 60
+        pos_y = 930
+        pos_x = 120
         position = (pos_x, pos_y)
 
         # Titulo
         font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 30)
         width, height = font.getsize(TITULO)
-        lines = textwrap.wrap(TITULO, width=20)
+        lines = textwrap.wrap(TITULO, width=45)
         for line in lines:
-            draw.text(position, line, font=font, fill="#662F8E", stroke_width=1, stroke_fill="#662F8E")
+            draw.text(position, line, font=font, fill="#000", stroke_width=0)
             pos_y += height
             position = (pos_x, pos_y)
 
         # Preço
-        position = (460, 105)
-        font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 40)
-        draw.text(position, PRECO, font=font, fill="#662F8E", stroke_width=1, stroke_fill="#662F8E")
+        position = (120, 1080)
+        font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 60)
+        draw.text(position, PRECO, font=font, fill="#000", stroke_width=1, stroke_fill="#000")
         
         #Preço capa
-        pos_y = 150
-        pos_x = 460
-        position = (pos_x, pos_y)
-        font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 30)
-        width, height = font.getsize(PRECO_CAPA)
-        draw.text(position, PRECO_CAPA, font=font, fill="#662F8E")
+        # pos_y = 150
+        # pos_x = 460
+        # position = (pos_x, pos_y)
+        # font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 30)
+        # width, height = font.getsize(PRECO_CAPA)
+        # draw.text(position, PRECO_CAPA, font=font, fill="#662F8E")
 
-        pos_y += (height / 1.5)
-        pos_xf = pos_x + width
-        draw.line([(pos_x, pos_y), (pos_xf, pos_y)], fill="#662F8E", width=3)
+        # pos_y += (height / 1.5)
+        # pos_xf = pos_x + width
+        # draw.line([(pos_x, pos_y), (pos_xf, pos_y)], fill="#662F8E", width=3)
 
         # Desconto
-        position = (460, 185)
-        font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 25)
-        draw.text(position, DESCONTO, font=font, fill="#662F8E")
+        # position = (460, 185)
+        # font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 25)
+        # draw.text(position, DESCONTO, font=font, fill="#662F8E")
 
-        # Descrição
-        pos_y = 770
-        pos_x = 60
-        position = (pos_x, pos_y)
+        # # Descrição
+        # pos_y = 770
+        # pos_x = 60
+        # position = (pos_x, pos_y)
 
-        font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 30)
-        width, height = font.getsize(DESCRICAO)
-        lines = textwrap.wrap(DESCRICAO, width=40)
-        for line in lines:
-            draw.text(position, line, font=font, fill="#662F8E", align='center')
-            pos_y += height
-            position = (pos_x, pos_y)
+        # font = ImageFont.truetype('./src/python/res/SEGUISB.TTF', 30)
+        # width, height = font.getsize(DESCRICAO)
+        # lines = textwrap.wrap(DESCRICAO, width=40)
+        # for line in lines:
+        #     draw.text(position, line, font=font, fill="#662F8E", align='center')
+        #     pos_y += height
+        #     position = (pos_x, pos_y)
 
         imgcp.save('./src/python/res/img_final.jpg')
 
         return True
-    except:
+    except Exception as ex:
         print('Erro: Erro ao montar a imagem')
+        print(ex)
         return False
 
 def post_story():
     global cl
     print('storie')
-    #cl.photo_upload_to_story("./src/python/res/img_final.jpg", "", links=[StoryLink(webUri=LINK, x=0.29, y=0.84, width=0.3, height=0.05)])
+    print(LINK)
+    cl.photo_upload_to_story("./src/python/res/img_final.jpg", "", links=[StoryLink(webUri=LINK, x=0.29, y=0.84, width=0.3, height=0.05)])
 
 def main():
-    result = login()
-    if result:
-        result = save_image()
+    # result = login()
+    # if result:
+    save_image()
 
-    if result: 
-        post_story()
+    # if result: 
+    #     post_story()
 
     print('finalizou')
 
